4015c04a050d709b0262b9d9d13f380d76401956
KaitlynC <katybear@hotmail.com>
2024-08-26 18:18:58 -0700

    apple vscode

---
+++ Javascriptpractice.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 5; i++) {
+  //text += "The number is " + i + "<br>";
+  console.log(i)
+}
+
+
+const printFive = () => {   
+    for (let i = 0; i < 5; i++) {
+    //text += "The number is " + i + "<br>";
+    console.log(i)
+  }
+
+}
+
+
+printFive()
